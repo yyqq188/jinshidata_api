85602d1900b772923bcaf9d46c2b4e87ffbfdbe9
yyqq188 <83791248@qq.com>
2023-09-08 15:04:46 +0800

    change for render

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from routers.economics_router import router as economics_router
 from routers.event_router import router as event_router
 from routers.holiday_router import router as holiday_router
 from fastapi import Request
+from pathlib import Path
+import os
+#部署到render时,需要注释该行
+# load_dotenv(Path(Path.home(),"env_config/.env_jinshidata_api"),override=True)
 
-config = dotenv_values(".env")
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_db_client():
-  app.mongodb_client = MongoClient(config['MONGO_URI'])
-  app.database = app.mongodb_client[config["MONGO_DATABASE"]]
+  app.mongodb_client = MongoClient(os.getenv("MONGO_URI"))
+  app.database = app.mongodb_client[os.getenv("MONGO_DATABASE")]
   print("Connected mongodb")
 
 @app.on_event("shutdown")
