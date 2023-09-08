340b21e0ddea566ef60170c4fbe5ab89c965ee0e
yyqq188 <83791248@qq.com>
2023-09-08 15:39:08 +0800

    syntax error

--- routers/holiday_router.py
+++ routers/holiday_router.py
@@ -11,8 +11,8 @@ def list_holiday(request:Request):
   holidays = list(request.app.database["scrapy_items"].find(limit=100))
   return holidays
 
-@router.get("/{id}",response_description="get a single holiday info by id",response_model=Holiday)
-def find_holiday(id:str,request:Request):
-  if(holiday := request.app.database["scrapy_items"].find_one({"_id":id}))  is not None :
-    return holiday
-  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f"Event with ID {id} not found")
+# @router.get("/{id}",response_description="get a single holiday info by id",response_model=Holiday)
+# def find_holiday(id:str,request:Request):
+#   if(holiday := request.app.database["scrapy_items"].find_one({"_id":id}))  is not None :
+#     return holiday
+#   raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail = f"Event with ID {id} not found")
